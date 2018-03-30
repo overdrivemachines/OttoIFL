45501f86c3c824e677b7888e7243fea82ec009dc
Dipen Chauhan <get.dipen@gmail.com>
2018-03-30 14:26:37 -0700

    Initial commit

---
+++ examples/OttoIFL_Test/OttoIFL_Test.ino
@@ -0,0 +1,17 @@
+#include <OttoIFL.h>
+
+Otto otto;
+
+void setup() 
+{  
+  // put your setup code here, to run once:
+  otto.init();
+  
+
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+}
