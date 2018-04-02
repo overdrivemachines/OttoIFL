a92de87fe9a528facaddae62e84d8b5c287b7302
Scotty Franzyshen <sfranzyshen@gmail.com>
2018-04-01 18:39:40 -0700

    unifying SoftSerial & OttoSerial
    this combines the separate real serial and softserial otto serial commands into a single stream ... no need to switch code any more

--- src/OttoSerialCommand.h
+++ src/OttoSerialCommand.h
@@ -15,11 +15,8 @@
 #include "WProgram.h"
 #endif
 
-
-
-
 #include <string.h>
-
+#include <SoftwareSerial.h>  
 
 #define SERIALCOMMANDBUFFER 35  //16 after changed by me
 #define MAXSERIALCOMMANDS	14
@@ -28,7 +25,7 @@
 class OttoSerialCommand
 {
 	public:
-		OttoSerialCommand();      // Constructor
+		OttoSerialCommand(SoftwareSerial &SoftSer);      // Constructor
 
 		void clearBuffer();   // Sets the command buffer to all '\0' (nulls)
 		char *next();         // returns pointer to next token found in command buffer (for getting arguments to commands)
@@ -47,11 +44,12 @@ class OttoSerialCommand
 		typedef struct _callback {
 			char command[SERIALCOMMANDBUFFER];
 			void (*function)();
-		} OttoSerialCommandCallback;            // Data structure to hold Command/Handler function key-value pairs
+		} OttoSerialCommandCallback;        // Data structure to hold Command/Handler function key-value pairs
 		int numCommand;
 		OttoSerialCommandCallback CommandList[MAXSERIALCOMMANDS];   // Actual definition for command/handler array
 		void (*defaultHandler)();           // Pointer to the default handler function 
-		int usingOttoSoftwareSerial;            // Used as boolean to see if we're using OttoSoftwareSerial object or not
+		SoftwareSerial *SoftSerial;         // Pointer to a user-created SoftwareSerial object
+		int usingOttoSoftwareSerial;        // Used as boolean to see if we're using OttoSoftwareSerial object or not
 
 };
 
